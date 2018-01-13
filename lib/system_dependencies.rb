49c05e76ed140af0182277cc06874f5ddc5a577d
darcang <xxbadimxx@gmail.com>
2018-01-13 18:39:48 +0200

    feature System Dependencies

--- lib/system_dependencies.rb
+++ lib/system_dependencies.rb
@@ -1,25 +1,29 @@
 module Agile
   class SystemDependencies
     def initialize(input_file)
-      @input_file = parse_file File.new(input_file)
+      @parsed_file = parse_file(File.new(input_file))
     end
 
     def perform
-      input_file.map do |line|
-        puts line
-        args_array = line.split
-        command = args_array.shift
-        args = args_array.first if args_array.size == 1
-        Agile::Commands.public_send(command.downcase, args)
+      parsed_file.map do |line|
+        process_the_line(line)
       end
     end
 
     private
 
-      attr_reader :input_file
+      attr_reader :parsed_file
 
       def parse_file(sample_file)
         sample_file.readlines
+      end
+
+      def process_the_line(line)
+        puts line
+        args = line.split
+        command = args.shift
+        args = args.first if args.size == 1
+        Agile::Commands.public_send(command.downcase, args)
       end
   end
 end
